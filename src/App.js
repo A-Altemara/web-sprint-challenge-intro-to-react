48fc74352b94774ba207d6ca6bfae49e4a36ca18
Airiel Altemara <vsokord#gmail.com>
2022-12-28 16:40:33 -0800

    character function working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {MappedCharacters, Character} from './components/Character';
 
 const App = () => {
+  const [char, setChar] = useState({})
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,9 +11,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+useEffect(() => {
+  axios.get(`https://swapi.dev/api/people/`)
+  .then(res => {
+    console.log(res.data)
+    setChar(res.data)
+  })
+  .catch(err => console.error(err))
+}, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+        <MappedCharacters char={char}/>
+        <Character char={char[0]}/>
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from "react";
+
+export function Character(props) {
+    console.log('Character')
+    console.log(props)
+    return (
+        <>
+        <h2>{props.char.name}</h2>
+        <h3>Gender: {props.char.gender}</h3>
+        <h3>Height: {props.char.height}</h3>
+        <h3>Mass: {props.char.mass}</h3>
+        <h3>Birth Year: {props.char.birth_year}</h3>
+        <h3>Eye Color: {props.char.eye_color}</h3>
+        <h3>Hair Color: {props.char.hair_color}</h3>
+        <h3>Skin Color: {props.char.skin_color}</h3>
+        </>
+        )
+}
+
+export function MappedCharacters({char}){
+    console.log('Mapped')
+    console.log(char)
+    return null
+    // char.map((ch, idx) => <li> {Character(ch)} key={idx}</li>)
+}
+
+
+export default MappedCharacters
